eeef0c0af2c8fe02e0d42e324b4a1a6947e2c152
pxlhack <nellsouhd@gmail.com>
2023-06-14 18:22:20 +0700

    first commit

---
+++ src/main/java/ru/pxlhack/geniustwin/GeniusTwinApplication.java
@@ -0,0 +1,13 @@
+package ru.pxlhack.geniustwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeniusTwinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeniusTwinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/pxlhack/geniustwin/GeniusTwinApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pxlhack.geniustwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeniusTwinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
